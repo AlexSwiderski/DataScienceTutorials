06a532c000fc1c9ba062d4e6fc379f5eda3720b4
Alex Swiderski <alexanderswiderski@gmail.com>
2021-01-11 16:23:57 -0500

    created

---
+++ Bayotas/r4ds12_17.R
@@ -0,0 +1,3 @@
+# If you put the # before a line of code, it will not run and you can write whatever you want. 
+# This file is called an R script.  You can save the file and it will save all your code and comments, 
+# such as the one we are writing now. 
